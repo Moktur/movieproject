c6fb333a268ffe1cfeafd9825aed809a0bafb3da
pepe <p.zajonz@posteo.de>
2025-07-13 05:25:07 +0200

    updated list_movies() function to retrieve url

--- movie_storage_sql.py
+++ movie_storage_sql.py
@@ -24,9 +24,13 @@ with engine.connect() as connection:
 def list_movies():
   """Retrieve all movies from the database."""
   with engine.connect() as connection:
-    result = connection.execute(text("SELECT title, year, rating FROM movies"))
+    result = connection.execute(text("SELECT title, year, rating, url FROM movies"))
     movies = result.fetchall()
-    return {row[0]: {"year": row[1], "rating": row[2]} for row in movies}
+    return {row[0]: {
+      "year": row[1], 
+      "rating": row[2], 
+      "url": row[3]
+      } for row in movies}
 
 def add_movie(title, year, rating, url):
   """Add a new movie to the database."""
