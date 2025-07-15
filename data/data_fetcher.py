794bc877fe95926a382cfaf5dc49838f7084fb70
pepe <p.zajonz@posteo.de>
2025-07-15 08:15:29 +0200

    restored data-folder and python files because of accidently deleting

---
+++ data/data_fetcher.py
@@ -0,0 +1,19 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+def fetch_data(movie_search):
+  """
+  Fetches movie information from API.
+  Returns a dictionary with the requested movie information.
+  """
+  api_url = f'http://www.omdbapi.com/?t={movie_search}'
+  response = requests.get(api_url, API_KEY)
+  if response.status_code == requests.codes.ok:
+    return response.json()
+  else:
+    raise Exception(f"API Error: {response.status_code} - {response.text}")
+  
